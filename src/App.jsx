9ddfb3cc42be3a81caefe0d48b1f8c6d237134c3
Stephanie Morales P. <stephaniemdes2012@gmail.com>
2025-03-25 18:55:42 -0700

    Proyecto Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
-function App () {
+import ProfileCard from "./assets/ProfileCard"
+function App() {
 
   return (
     <>
-      <h1>Componente de App</h1>
+    <h1>compenente de app</h1>
+    <section className="hero is-dark">
+      <div className="hero-body">
+        <p className="title">Personal Digital Assistants</p>
+      </div>
+    </section>
+
+<div className="container">
+  <section className="section">
+    <div className="alexa">
+      <div className="@alexa24">
+        <ProfileCard />
+      </div>
+      <div className="column is-4">
+        <ProfileCard />
+        </div>
+        <div className="column is-4">
+        <ProfileCard />
+      </div>
+      </div>
+  </section>
+</div>
     </>
   )
 }
---
+++ src/assets/ProfileCard.jsx
@@ -0,0 +1,8 @@
+function ProfileCard(props) {
+    console.log(props)
+    return (
+        <div> Profile Card</div>
+    )
+}
+
+export default ProfileCard
